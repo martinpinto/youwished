06518667330a93c9d2170aeb41aa353f62a6d59e
Martin Pinto-Bazurco <martinpintob@gmail.com>
2016-07-29 09:52:05 +0200

    Refactored client side code.

---
+++ src/server/helpers/database/persistence.js
@@ -0,0 +1,17 @@
+export interface PersistenceLayer {
+
+/*
+PersistedModel.count
+PersistedModel.create
+PersistedModel.destroyAll
+PersistedModel.destroyById
+PersistedModel.exists
+PersistedModel.find
+PersistedModel.findById
+PersistedModel.findOne
+PersistedModel.updateAll
+*/
+
+} 
+
+
